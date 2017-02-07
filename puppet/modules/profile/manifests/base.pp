1e4e6c180c2cb3284148f57bd7c26aa1211931b5
Matt Stone <matt.stone@gettyimages.com>
2017-02-07 07:11:52 -0800

    commas

--- puppet/modules/profile/manifests/base.pp
+++ puppet/modules/profile/manifests/base.pp
@@ -4,15 +4,15 @@ class profile::base {
     update => {
       frequency => 'daily',
     },
-    before => Package[$packages]
+    before => Package[$packages],
   }
   exec { 'apt upgrade':
-    command     => '/bin/sh apt-get upgrade -y'
+    command     => '/bin/sh apt-get upgrade -y',
     refreshonly => true,
-    subscribe   => Class['apt']
+    subscribe   => Class['apt'],
   }
   class { 'locales':
-    default_locale => 'en_US.UTF-8'
+    default_locale => 'en_US.UTF-8',
     locales   => ['en_US.UTF-8 UTF-8'],
   }
   package { $packages :
