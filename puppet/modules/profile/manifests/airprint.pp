7db9c7c3aed0d0938b581ecf79453b6b7bdc3c4d
Matt Stone <matt.stone@gettyimages.com>
2017-02-07 07:30:56 -0800

    cups

--- puppet/modules/profile/manifests/airprint.pp
+++ puppet/modules/profile/manifests/airprint.pp
@@ -30,25 +30,8 @@ class profile::airprint {
         ensure  => running,
         require => Package[$packages],
     }
-    file_line { 'cups port':
-        ensure => present,
-        path   => '/etc/cups/cupsd.conf',
-        line   => 'Port 631',
-        match  => '^Listen localhost:631',
-        before => Service['cups']
-    }
-    file_line { 'cups serveralias':
-        ensure => present,
-        path   => '/etc/cups/cupsd.conf',
-        line   => 'ServerAlias *',
-        after  => '^DefaultAuthType Basic$',
-        before => Service['cups'],
-    }
-    file_line { 'cups order':
-        ensure => present,
-        path   => '/etc/cups/cupsd.conf',
-        line   => 'Allow @Local',
-        after  => '^  Order allow,deny$',
-        before => Service['cups'],
+    file { '/etc/cups/cupsd.conf' :
+        ensure  => file,
+        content => file('puppet:///${module_name}/cupsd.conf'),
     }
 }
