ab9510b1094a262d85e66ce93daab26d9316a319
Matt Stone <matt.stone@gettyimages.com>
2017-02-07 13:06:05 -0800

    path

--- get_modules.py
+++ get_modules.py
@@ -4,7 +4,7 @@ import subprocess
 
 cwd = os.getcwd()
 modpath = '{}/puppet/modules'.format(cwd)
-with open('{}/resources/airprint.yaml'.format(cwd)) as y:
+with open('{}/resources.yaml'.format(cwd)) as y:
     mod = yaml.load(y)
     for module in mod['puppet_modules']:
         subprocess.call('/usr/bin/puppet module install {} --modulepath {}'.format(module, modpath), shell=True)
