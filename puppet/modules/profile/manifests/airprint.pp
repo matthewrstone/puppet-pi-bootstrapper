4a5831385fc8953c38699cab015ed453447e3d7d
Matt Stone <matt.stone@gettyimages.com>
2017-02-07 07:25:31 -0800

    config

--- puppet/modules/profile/manifests/airprint.pp
+++ puppet/modules/profile/manifests/airprint.pp
@@ -32,21 +32,21 @@ class profile::airprint {
     }
     file_line { 'cups port':
         ensure => present,
-        path   => '/etc/cups/cupsd.config',
+        path   => '/etc/cups/cupsd.conf',
         line   => 'Port 631',
         match  => '^Listen localhost:631',
         before => Service['cups']
     }
     file_line { 'cups serveralias':
         ensure => present,
-        path   => '/etc/cups/cupsd.config',
+        path   => '/etc/cups/cupsd.conf',
         line   => 'ServerAlias *',
         after  => '^DefaultAuthType Basic$',
         before => Service['cups'],
     }
     file_line { 'cups order':
         ensure => present,
-        path   => '/etc/cups/cupsd.config',
+        path   => '/etc/cups/cupsd.conf',
         line   => 'Allow @Local',
         after  => '^  Order allow,deny$',
         before => Service['cups'],
